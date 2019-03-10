aaced4163f9cf99908346e447e8b805dd6e9bcb5
barman47 <nomsouzoanya@yahoo.co.uk>
2019-03-10 16:08:48 +0100

    Ready for first deploy

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const path = require('path')
 const app = express();
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
